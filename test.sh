0fb4d65744ca305ec8dd425a6eb28fed898550b6
dreamjh1111 <75324311+dreamjh1111@users.noreply.github.com>
2021-11-25 14:51:24 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 cd ~/vi_setting && cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-rm -rf ~/vi_setting
 cd ~
+rm -rf ~/vi_setting
